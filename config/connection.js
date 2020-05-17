8a46ca6105d72aa80dd6a47ed99b8023112fa921
Stephen Settle <ssettle316@gmail.com>
2020-05-17 14:21:50 -0400

    final version of burger homework

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require("mysql");
+require('dotenv').config();
 
 //1)create connection object
-var connection = mysql.createConnection({
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "heights06",
     database: "burgers_db"
   });
+}
 
 //2)connect
 connection.connect(function(err) {
